2e1edfac47f2bb255d3dc056fffcbea2138b2b77
KouKen <platiniridium@gmail.com>
2019-09-22 15:30:58 +0900

    inital

---
+++ mychat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  mychat
+//
+//  Created by 黄健 on 2019/09/16.
+//  Copyright © 2019 Kou Ken. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mychat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  mychat
+//
+//  Created by 黄健 on 2019/09/16.
+//  Copyright © 2019 Kou Ken. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
